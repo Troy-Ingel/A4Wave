3b64a229824e51679c6571a805b1842085187687
Michael Woo <michaelwoo120@gmail.com>
2017-10-09 12:30:13 -0400

    Nice Job Evan: Dynamic resizing on the game and maximumize the frame

--- src/mainGame/Game.java
+++ src/mainGame/Game.java
@@ -2,12 +2,17 @@ package mainGame;
 
 import java.awt.Canvas;
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Toolkit;
 import java.awt.image.BufferStrategy;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 /**
  * Main game class. This class is the driver class and it follows the Holder
- * pattern. It houses references to ALL of the components of the game
+ * pattern. It houses references to ALL of the components of the gamee
  * 
  * @author Brandon Loehle 5/30/16
  */
@@ -16,8 +21,11 @@ public class Game extends Canvas implements Runnable {
 
 	private static final long serialVersionUID = 1L;
 
-	public static final int WIDTH = 1250, HEIGHT = 700;
-	// J set to W1250, H700 (baby computer screen size) for HUD fix
+	// EVAN CHANGES: will first use toolkit to find the screen size, then will
+	// set width and height to sooo
+	private static Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+	public static final int WIDTH = (int) screenSize.getWidth(), HEIGHT = (int) screenSize.getHeight();
+
 	private Thread thread;
 	private boolean running = false;
 
@@ -63,8 +71,8 @@ public class Game extends Canvas implements Runnable {
 	}
 
 	/**
-	 * The thread is simply a programs path of execution. This method ensures that
-	 * this thread starts properly.
+	 * The thread is simply a programs path of execution. This method ensures
+	 * that this thread starts properly.
 	 */
 	public synchronized void start() {
 		thread = new Thread(this);
@@ -120,38 +128,49 @@ public class Game extends Canvas implements Runnable {
 	}
 
 	/**
-	 * Constantly ticking (60 times per second, used for updating smoothly). Used
-	 * for updating the instance variables (DATA) of each entity (location, health,
-	 * appearance, etc).
+	 * Constantly ticking (60 times per second, used for updating smoothly).
+	 * Used for updating the instance variables (DATA) of each entity (location,
+	 * health, appearance, etc).
 	 */
 	private void tick() {
 		handler.tick();// ALWAYS TICK HANDLER, NO MATTER IF MENU OR GAME SCREEN
 		if (gameState == STATE.Game) {// game is running
 			hud.tick();
-			if (Spawn1to10.LEVEL_SET == 1) {// user is on levels 1 thru 10, update them
+			if (Spawn1to10.LEVEL_SET == 1) {// user is on levels 1 thru 10,
+											// update them
 				spawner.tick();
-			} else if (Spawn1to10.LEVEL_SET == 2) {// user is on levels 10 thru 20, update them
+			} else if (Spawn1to10.LEVEL_SET == 2) {// user is on levels 10 thru
+													// 20, update them
 				spawner2.tick();
 			}
-		} else if (gameState == STATE.Menu || gameState == STATE.Help) {// user is on menu, update the menu items
+		} else if (gameState == STATE.Menu || gameState == STATE.Help) {// user
+																		// is on
+																		// menu,
+																		// update
+																		// the
+																		// menu
+																		// items
 			menu.tick();
-		} else if (gameState == STATE.Upgrade) {// user is on upgrade screen, update the upgrade screen
+		} else if (gameState == STATE.Upgrade) {// user is on upgrade screen,
+												// update the upgrade screen
 			upgradeScreen.tick();
-		} else if (gameState == STATE.GameOver) {// game is over, update the game over screen
+		} else if (gameState == STATE.GameOver) {// game is over, update the
+													// game over screen
 			gameOver.tick();
 		}
 
 	}
 
 	/**
-	 * Constantly drawing to the many buffer screens of each entity requiring the
-	 * Graphics objects (entities, screens, HUD's, etc).
+	 * Constantly drawing to the many buffer screens of each entity requiring
+	 * the Graphics objects (entities, screens, HUD's, etc).
 	 */
 	private void render() {
 
 		/*
-		 * BufferStrategies are used to prevent screen tearing. In other words, this
-		 * allows for all objects to be redrawn at the same time, and not individually
+		 * BufferStrategies are used to prevent screen tearing. In other words,
+		 * this allows for all objects to be redrawn at the same time, and not
+		 * individually
 		 */
 		BufferStrategy bs = this.getBufferStrategy();
 		if (bs == null) {
@@ -166,16 +185,28 @@ public class Game extends Canvas implements Runnable {
 		g.setColor(Color.black);
 		g.fillRect(0, 0, (int) WIDTH, (int) HEIGHT);
 
-		handler.render(g); // ALWAYS RENDER HANDLER, NO MATTER IF MENU OR GAME SCREEN
+		handler.render(g); // ALWAYS RENDER HANDLER, NO MATTER IF MENU OR GAME
+							// SCREEN
 
 		if (gameState == STATE.Game) {// user is playing game, draw game objects
 			hud.render(g);
-		} else if (gameState == STATE.Menu || gameState == STATE.Help) {// user is in help or the menu, draw the menu
-																		// and help objects
+		} else if (gameState == STATE.Menu || gameState == STATE.Help) {// user
+																		// is in
+																		// help
+																		// or
+																		// the
+																		// menu,
+																		// draw
+																		// the
+																		// menu
+			// and help objects
 			menu.render(g);
-		} else if (gameState == STATE.Upgrade) {// user is on the upgrade screen, draw the upgrade screen
+		} else if (gameState == STATE.Upgrade) {// user is on the upgrade
+												// screen, draw the upgrade
+												// screen
 			upgradeScreen.render(g);
-		} else if (gameState == STATE.GameOver) {// game is over, draw the game over screen
+		} else if (gameState == STATE.GameOver) {// game is over, draw the game
+													// over screen
 			gameOver.render(g);
 		}
 
@@ -186,8 +217,8 @@ public class Game extends Canvas implements Runnable {
 
 	/**
 	 * 
-	 * Constantly checks bounds, makes sure players, enemies, and info doesn't leave
-	 * screen
+	 * Constantly checks bounds, makes sure players, enemies, and info doesn't
+	 * leave screen
 	 * 
 	 * @param var
 	 *            x or y location of entity
--- src/mainGame/Window.java
+++ src/mainGame/Window.java
@@ -7,28 +7,29 @@ import javax.swing.JFrame;
 
 /**
  * Loads the window of the game, and sets the proper dimensions
- * @author Brandon Loehle
- * 5/30/16
+ * 
+ * @author Brandon Loehlee 5/30/16
  */
 
-public class Window extends Canvas{
+public class Window extends Canvas {
 
 	private static final long serialVersionUID = 1L;
-	
-	public Window(int width, int height, String title, Game game){
+
+	public Window(int width, int height, String title, Game game) {
 		JFrame frame = new JFrame(title);
 		frame.setPreferredSize(new Dimension(width, height));
 		frame.setMaximumSize(new Dimension(width, height));
 		frame.setMinimumSize(new Dimension(width, height));
-		
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setResizable(true);
 		frame.setLocationRelativeTo(null);
+		// EVAN CHANGES: will take frame to maximize the x and y so that game is
+		// full screeen not matter the device
+		frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+		frame.setUndecorated(true);
 		frame.add(game);
 		frame.setVisible(true);
 		game.start();
-		
 	}
-
 
 }
