d12b6e55f6e9f181bfe9c5292216857937a28181
......... <JJShevchenko@quinnipiac.edu>
2017-11-09 20:40:17 -0500

    for Michael and Troy...

--- src/mainGame/MouseListener.java
+++ src/mainGame/MouseListener.java
@@ -117,7 +117,7 @@ public class MouseListener extends MouseAdapter {
 
 			}
 			// Leaderboard Button
-			// Should be fixed now!
+			// Is this working?
 			else if (mouseOver(mx, my, 100, Game.HEIGHT / 2 - 65, Game.WIDTH - 200, 100)) {
 				// The leaderboard has been clicked
 				AudioPlayer.getSound("sound").play();
